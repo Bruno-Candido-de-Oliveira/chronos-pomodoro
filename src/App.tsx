f8c9f01771e3823cd776416b7a24fdb2d91d0a6f
Bruno <134114980+Bruno-Candido-de-Oliveira@users.noreply.github.com>
2026-03-09 22:58:30 -0300

    primeiro commit da aplicação pomodoro

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import '../styles/theme.css'
+import '../styles/global.css'
+
+export function App() {
+  console.log('oi');
+
+  return (
+    <>
+      <h1>Olá, mundo! (do app)</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui cum,
+        minima eaque natus vel explicabo labore ea architecto, totam quae aut
+        unde quisquam debitis tempora repellendus ipsam nam? Sint, temporibus?
+      </p>
+    </>
+  );
+}
