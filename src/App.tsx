5ebb6ab01277e4b11b936844546a7a3f69a24c62
Bruno <134114980+Bruno-Candido-de-Oliveira@users.noreply.github.com>
2026-03-15 21:42:33 -0300

    Adição de estilos para o componente Heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import '../styles/theme.css'
-import '../styles/global.css'
+import { Heading } from './components/Heading';
+import '../styles/global.css';
+import '../styles/theme.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   console.log('oi');
 
   return (
     <>
-      <h1>Olá, mundo! (do app)</h1>
+      <Heading>
+        Bruno Candido
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui cum,
         minima eaque natus vel explicabo labore ea architecto, totam quae aut
