34c977da7e9ac910e5fb2e4a9b747534f8a9c341
bruno-candido-forcetech <bruno.oliveira@forcetechjr.com.br>
2026-03-15 22:46:42 -0300

    Organização de arquivos; criação de arquivos de container

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import { Heading } from './components/Heading';
 import '../styles/global.css';
 import '../styles/theme.css';
-import { TimerIcon } from 'lucide-react';
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 export function App() {
   console.log('oi');
 
   return (
     <>
-      <Heading>
-        Bruno Candido
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui cum,
-        minima eaque natus vel explicabo labore ea architecto, totam quae aut
-        unde quisquam debitis tempora repellendus ipsam nam? Sint, temporibus?
-      </p>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
     </>
   );
 }
